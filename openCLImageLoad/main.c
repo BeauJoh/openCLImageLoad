27417ba1155450ef3ccc5d31a35973cebf471c7e
Beau Johnston <beau@MacBook-Beau.local>
2011-04-27 11:29:17 +1000

    Initial Commit

---
+++ openCLImageLoad/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  openCLImageLoad
+//
+//  Created by Beau Johnston on 27/04/11.
+//  Copyright 2011 University Of New England. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
