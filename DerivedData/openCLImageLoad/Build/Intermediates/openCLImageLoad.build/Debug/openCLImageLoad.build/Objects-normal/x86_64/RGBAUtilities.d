02abb23cde0e13ba1ff1dd4516bcedd45dc4cbd9
Beau Johnston <beau@inbeta.org>
2011-08-25 14:18:32 +1000

    Base commit

--- DerivedData/openCLImageLoad/Build/Intermediates/openCLImageLoad.build/Debug/openCLImageLoad.build/Objects-normal/x86_64/RGBAUtilities.d
+++ DerivedData/openCLImageLoad/Build/Intermediates/openCLImageLoad.build/Debug/openCLImageLoad.build/Objects-normal/x86_64/RGBAUtilities.d
@@ -1,3 +1,5 @@
 dependencies: \
   /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/RGBAUtilities.cpp \
-  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/RGBAUtilities.h
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/RGBAUtilities.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/libpng.framework/Headers/png.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/libpng.framework/Headers/pngconf.h
--- DerivedData/openCLImageLoad/Build/Intermediates/openCLImageLoad.build/Debug/openCLImageLoad.build/Objects-normal/x86_64/main.d
+++ DerivedData/openCLImageLoad/Build/Intermediates/openCLImageLoad.build/Debug/openCLImageLoad.build/Objects-normal/x86_64/main.d
@@ -1,5 +1,12 @@
 dependencies: \
   /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/main.cpp \
   /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/openCLUtilities.h \
-  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/FreeImage.h \
-  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/RGBAUtilities.h
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/openCLImageLoad/RGBAUtilities.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/libpng.framework/Headers/png.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/libpng.framework/Headers/pngconf.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/opencl.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/cl.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/cl_platform.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/cl_gl.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/cl_gl_ext.h \
+  /Users/beau/Documents/FourthYear/miscProjects/openCLImageLoad/dylibsAndFrameworks/OpenCL.framework/Headers/cl_ext.h
